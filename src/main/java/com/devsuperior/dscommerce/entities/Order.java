799db261e2dd6585adf3092ab8c70e57da3743ba
Vinicius Prado <viniciusprado.prado@gmail.com>
2024-06-16 00:06:18 -0300

    payment

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -19,4 +19,57 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order(){
+    }
+
+    public Order(Long id, Payment payment, User client, OrderStatus status, Instant moment) {
+        this.id = id;
+        this.payment = payment;
+        this.client = client;
+        this.status = status;
+        this.moment = moment;
+    }
+
+    public Long getId() { 
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
